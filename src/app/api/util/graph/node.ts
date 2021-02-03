49df4072d62e1144ae5dc2aae9041f1b8d7a1537
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-02-03 15:02:20 +0100

    first graph i am mostly satisfied with

--- src/app/api/util/graph/node.ts
+++ src/app/api/util/graph/node.ts
@@ -2,10 +2,12 @@ export class Node {
   id!: string;
   label!: string;
   subLabel!: string;
+  neededBuilding!: number;
 
-  constructor(id: string, label: string, subLabel: string) {
+  constructor(id: string, label: string, subLabel: string, neededBuilding: number) {
     this.id = id;
     this.label = label;
     this.subLabel = subLabel;
+    this.neededBuilding = neededBuilding;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import {AllComponents} from './api/model/components/allComponents';
 import {ASSEMBLER, RAW_OIL, WATER} from './api/util/constants/names';
 import {Input} from './api/util/input';
 import {Result} from './api/util/result';
-import {Tree} from './api/util/tree';
 import {Node} from './api/util/graph/Node';
 import {Link} from './api/util/graph/link';
 
@@ -101,30 +100,71 @@ export class AppComponent {
   builtResultGraph() {
     this.nodes = [];
     this.links = [];
+    let allLinks: Link[] = [];
 
+    let nodeMap: Map<string, Node> = new Map<string, Node>();
+
+    //create all nodes
     let alreadyAdded = false;
     for (let result of this.results) {
+      let nodeMapKeys: string[] = [];
+
+      for (let key of nodeMap.keys()) {
+        nodeMapKeys.push(key);
+      }
+
+      if (nodeMapKeys.indexOf(result.item.name) > 0) {
+        let currentNode = <Node>nodeMap.get(result.item.name);
+        let neededBuildings = currentNode.neededBuilding + result.neededBuildingsDisplay;
+
+        if (result.item.baseItem) {
+          nodeMap.set(result.item.name, new Node(currentNode.id, currentNode.label, neededBuildings.toFixed(2) + 'x ' + result.item.veinType, neededBuildings))
+        } else {
+          nodeMap.set(result.item.name, new Node(currentNode.id, currentNode.label, neededBuildings.toFixed(2) + 'x ' + result.item.neededMachine, neededBuildings))
+        }
+      } else {
+        if (result.item.baseItem) {
+          nodeMap.set(result.item.name, new Node(result.item.name, result.item.name, result.neededBuildingsDisplay.toFixed(2) + 'x ' + result.item.veinType, result.neededBuildingsDisplay));
+        } else {
+          nodeMap.set(result.item.name, new Node(result.item.name, result.item.name, result.neededBuildingsDisplay.toFixed(2) + 'x ' + result.item.neededMachine, result.neededBuildingsDisplay));
+        }
+      }
+    }
+
+    // fill nodesList
+    for (let key of nodeMap.keys()) {
+      let newNode = <Node>nodeMap.get(key);
+      this.nodes.push(newNode);
+    }
+
+    // create all links
+    for (let i = 0; i < this.results.length; i++) {
+      let result = this.results[i];
+      if (result.nextItem.name) {
+        allLinks.push(new Link('l' + i, result.item.name, result.nextItem.name, result.generatedOutput.toString()));
+      }
+    }
+
+    // Combine links between the same nodes
+    let linkMap: Map<string, Link> = new Map<string, Link>();
+
+    for (let link of allLinks) {
       alreadyAdded = false;
-      for (let node of this.nodes) {
-        if (result.item.name === node.id) {
+      for (let key of linkMap.keys()) {
+        if (link.source === (<Link>linkMap.get(key)).source && link.target === (<Link>linkMap.get(key)).target) {
+          let newOutput: number = Number(link.label) + Number((<Link>linkMap.get(key)).label);
+          linkMap.set(key, new Link(link.id, link.source, link.target, newOutput.toString()));
           alreadyAdded = true;
         }
       }
 
       if (!alreadyAdded) {
-        if (result.item.baseItem) {
-          this.nodes.push(new Node(result.item.name, result.item.name, result.neededBuildingsDisplay.toFixed(2) + 'x ' + result.item.veinType));
-        } else {
-          this.nodes.push(new Node(result.item.name, result.item.name, result.neededBuildingsDisplay.toFixed(2) + 'x ' + result.item.neededMachine));
-        }
+        linkMap.set(link.id, link);
       }
     }
 
-    for (let i = 0; i < this.results.length; i++) {
-      let result = this.results[i];
-      if (result.nextItem.name) {
-        this.links.push(new Link('l' + i, result.item.name, result.nextItem.name, result.generatedOutput + '/min'));
-      }
+    for (let key of linkMap.keys()) {
+      this.links.push(<Link>linkMap.get(key));
     }
 
     this.nodes = [...this.nodes];
