3a5a9f0868b95a6b3df7e1bdf7f078c24bbbc7bc
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-02-04 19:18:29 +0100

    fix inconsistent constant names

--- src/app/api/model/util/constants/names.ts
+++ src/app/api/model/util/constants/names.ts
@@ -15,12 +15,12 @@ export const STONE_ORE = 'Stone Ore';
 export const COAL_ORE = 'Coal Ore';
 export const FRACTAL_SILICON = 'Fractal Silicon';
 export const KIMBERLITE_ORE = 'Kimberlite Ore';
-export const UNIPOLAR_MAGNET = 'Unipolar magnet';
+export const UNIPOLAR_MAGNET = 'Unipolar Magnet';
 export const OPTICAL_GRATING_CRYSTAL = 'Optical Grating Crystal';
 export const SPINIFORM_STALGMITE_CRYSTAL = 'Spiniform Stalagmite Crystal';
 export const RAW_OIL = 'Raw Oil';
 export const WATER = 'Water';
-export const FIRE_ICE = 'Fire ice';
+export const FIRE_ICE = 'Fire Ice';
 export const HYDROGEN = 'Hydrogen';
 export const REFINED_OIL = 'Refined Oil';
 
@@ -34,12 +34,12 @@ export const TITANIUM_VEIN = 'Titanium Veins';
 export const STONE_VEIN = 'Stone Veins';
 export const COAL_VEIN = 'Coal Veins';
 export const KIMBERLITE_VEIN = 'Kimberlite Veins';
-export const UNIPOLAR_MAGNET_VEIN = 'Unipolar magnet Veins';
+export const UNIPOLAR_MAGNET_VEIN = 'Unipolar Magnet Veins';
 export const OPTICAL_GRATING_CRYSTAL_VEIN = 'Optical Grating Crystal Veins';
 export const SPINIFORM_STALGMITE_CRYSTAL_VEIN = 'Spiniform Stalagmite Crystal Veins';
 export const FRACTAL_SILICON_VEIN = 'Fractal Silicon Veins';
 export const RAW_OIL_SPOT = 'Raw Oil Spots';
-export const FIRE_ICE_VEIN = 'Fire ice veins';
+export const FIRE_ICE_VEIN = 'Fire Ice Veins';
 
 // -----------------------------------------------------------------------------------------
 // Components
@@ -47,10 +47,10 @@ export const FIRE_ICE_VEIN = 'Fire ice veins';
 // 1
 export const IRON_INGOT = 'Iron Ingot';
 export const COPPER_INGOT = 'Copper Ingot';
-export const HIGH_PURITY_SILICON = 'High-purity silicon';
+export const HIGH_PURITY_SILICON = 'High-Purity Silicon';
 export const TITANIUM_INGOT = 'Titanium Ingot';
 export const STONE = 'Stone';
-export const ENERGETIC_GRAPHITE = 'Energetic graphite';
+export const ENERGETIC_GRAPHITE = 'Energetic Graphite';
 export const PLASMA_REFINING = '(Recipe) Plasma Refining';
 export const PLASTIC = 'Plastic';
 export const GRAPHENE = 'Graphene';
@@ -58,63 +58,63 @@ export const GRAPHENE = 'Graphene';
 // 2
 export const MAGNET = 'Magnet';
 export const MAGNETIC_COIL = 'Magnetic Coil';
-export const CRYSTAL_SILICON = 'Crystal silicon';
-export const TITANIUM_ALLOY = 'Titanium alloy';
+export const CRYSTAL_SILICON = 'Crystal Silicon';
+export const TITANIUM_ALLOY = 'Titanium Alloy';
 export const GLASS = 'Glass';
 export const DIAMOND = 'Diamond';
-export const X_RAY_CRACKING = '(Recipe) x-ray cracking';
+export const X_RAY_CRACKING = '(Recipe) X-Ray Cracking';
 export const ORGANIC_CRYSTAL = 'Organic Crystal';
-export const A_GRAPHENE = '(Recipe) Graphene (advanced)';
+export const A_GRAPHENE = '(Recipe) Graphene (Advanced)';
 export const HYDROGEN_FUEL_ROD = 'Hydrogen Fuel Rod';
 export const DEUTERON_FUEL_ROD = 'Deuteron Fuel Rod';
 
 // 3
 export const STEEL = 'Steel';
 export const ELECTRIC_MOTOR = 'Electric Motor';
-export const A_CRYSTAL_SILICON = '(Recipe) Crystal silicon (advanced)';
+export const A_CRYSTAL_SILICON = '(Recipe) Crystal Silicon (Advanced)';
 export const TITANIUM_GLASS = 'Titanium Glass';
 export const PRISMA = 'Prisma';
-export const A_DIAMOND = '(Recipe) Diamond (advanced)';
+export const A_DIAMOND = '(Recipe) Diamond (Advanced)';
 export const TITANIUM_CRYSTAL = 'Titanium Crystal';
 export const THRUSTER = 'Thruster';
 export const REINFORCED_THRUSTER = 'Reinforced Thruster';
-export const STRANGE_MATTER = 'Strange matter';
+export const STRANGE_MATTER = 'Strange Matter';
 
 // 4
 export const GEAR = 'Gear';
-export const ELECTROMAGNETIC_TURBINE = 'Electromagnetic turbine';
+export const ELECTROMAGNETIC_TURBINE = 'Electromagnetic Turbine';
 export const CIRCUIT_BOARD = 'Circuit Board';
-export const GRAVITON_LENS = 'Graviton lens';
-export const SULFURIC_ACID = 'sulfuric acid';
+export const GRAVITON_LENS = 'Graviton Lens';
+export const SULFURIC_ACID = 'Sulfuric Acid';
 export const DEUTERIUM = 'Deuterium';
-export const PLANE_FILTER = 'Plane filter';
-export const CARBON_NANOTUBE = 'Carbon nanotube';
-export const LOGISTICS_DRONE = 'Logistics drone';
-export const LOGISTICS_VESSEL = 'Logistics vessel';
-export const SMALL_CARRIER_ROCKET = 'Small carrier rocket';
-export const SILICON_ORE_FROM_STONE = 'Silicon ore from stone';
+export const PLANE_FILTER = 'Plane Filter';
+export const CARBON_NANOTUBE = 'Carbon Nanotube';
+export const LOGISTICS_DRONE = 'Logistics Drone';
+export const LOGISTICS_VESSEL = 'Logistics Vessel';
+export const SMALL_CARRIER_ROCKET = 'Small Carrier Rocket';
+export const SILICON_ORE_FROM_STONE = 'Silicon Ore (from Stone)';
 
 // 5
-export const PLASMA_EXCITER = 'Plasma exciter';
-export const SUPER_MAGNETIC_RING = 'super-magnetic ring';
-export const PARTICLE_BROADBAND = 'particle broadband';
-export const PROCESSOR = 'processor';
-export const CASIMIR_CRYSTAL = 'casimir crystal';
-export const PARTICLE_CONTAINER = 'particle container';
-export const SPACE_WARPER = 'space warper';
-export const A_CARBON_NANOTUBE = '(Recipe) Carbon nanotube (advanced)';
-export const SOLAR_SAIL = 'solar sail';
-export const FRAME_MATERIAL = 'frame material';
-export const DYSON_SPHERE_COMPONENT = 'dyson sphere component';
+export const PLASMA_EXCITER = 'Plasma Exciter';
+export const SUPER_MAGNETIC_RING = 'Super-Magnetic Ring';
+export const PARTICLE_BROADBAND = 'Particle Broadband';
+export const PROCESSOR = 'Processor';
+export const CASIMIR_CRYSTAL = 'Casimir Crystal';
+export const PARTICLE_CONTAINER = 'Particle Container';
+export const SPACE_WARPER = 'Space Warper';
+export const A_CARBON_NANOTUBE = '(Recipe) Carbon Nanotube (Advanced)';
+export const SOLAR_SAIL = 'Solar Sail';
+export const FRAME_MATERIAL = 'Frame Material';
+export const DYSON_SPHERE_COMPONENT = 'Dyson Sphere Component';
 
 // 6
-export const PHOTON_COMBINER = 'photon combiner';
-export const A_PHOTON_COMBINER = '(Recipe) photon combiner (advanced)';
-export const MICROCRYSTALLINE_COMPONENT = 'microcrystalline component';
-export const QUANTUM_CHIP = 'quantum chip';
-export const A_CASIMIR_CRYSTAL = '(Recipe) casimir crystal (advanced)';
-export const A_PARTICLE_CONTAINER = '(Recipe) particle container (advanced)';
-export const A_SPACE_WARPER = '(Recipe) space warper (advanced)';
+export const PHOTON_COMBINER = 'Photon Combiner';
+export const A_PHOTON_COMBINER = '(Recipe) Photon Combiner (Advanced)';
+export const MICROCRYSTALLINE_COMPONENT = 'Microcrystalline Component';
+export const QUANTUM_CHIP = 'Quantum Chip';
+export const A_CASIMIR_CRYSTAL = '(Recipe) Casimir Crystal (Advanced)';
+export const A_PARTICLE_CONTAINER = '(Recipe) Particle Container (Advanced)';
+export const A_SPACE_WARPER = '(Recipe) Space Warper (Advanced)';
 
 // 7
 export const ELECTROMAGNETIC_MATRIX = 'Electromagnetic Matrix';
@@ -128,46 +128,46 @@ export const FOUNDATION = 'Foundation';
 // Buildings
 // -----------------------------------------------------------------------------------------
 // 1
-export const TESLA_TOWER = 'Tesla tower';
-export const WIRELESS_POWER_TOWER = 'wireless power tower';
-export const SATELLITE_SUBSTATION = 'satellite substation';
-export const WIND_TURBINE = 'wind turbine';
-export const THERMAL_POWER_STATION = 'thermal power station';
-export const SOLAR_PANEL = 'solar panel';
-export const ACCUMULATOR = 'accumulator';
-export const RAY_RECEIVER = 'ray receiver';
-export const MINI_FUSION_POWER_STATION = 'mini fusion power station';
-export const ENERGY_EXCHANGER = 'energy exchanger';
+export const TESLA_TOWER = 'Tesla Tower';
+export const WIRELESS_POWER_TOWER = 'Wireless Power Tower';
+export const SATELLITE_SUBSTATION = 'Satellite Substation';
+export const WIND_TURBINE = 'Wind Turbine';
+export const THERMAL_POWER_STATION = 'Thermal Power Station';
+export const SOLAR_PANEL = 'Solar Panel';
+export const ACCUMULATOR = 'Accumulator';
+export const RAY_RECEIVER = 'Ray Receiver';
+export const MINI_FUSION_POWER_STATION = 'Mini Fusion Power Station';
+export const ENERGY_EXCHANGER = 'Energy Exchanger';
 
 // 2
-export const CONVEYOR_BELT_MK_1 = 'Conveyor Belt MK. 1';
-export const CONVEYOR_BELT_MK_2 = 'Conveyor Belt MK. 2';
-export const CONVEYOR_BELT_MK_3 = 'Conveyor Belt MK. 3';
-export const SPLITTER = 'Splitter(4-direction)';
-export const STORAGE_MK_1 = 'Storage MK. 1';
-export const STORAGE_MK_2 = 'Storage MK. 2';
+export const CONVEYOR_BELT_MK_1 = 'Conveyor Belt Mk. 1';
+export const CONVEYOR_BELT_MK_2 = 'Conveyor Belt Mk. 2';
+export const CONVEYOR_BELT_MK_3 = 'Conveyor Belt Mk. 3';
+export const SPLITTER = 'Splitter(4-Direction)';
+export const STORAGE_MK_1 = 'Storage Mk. 1';
+export const STORAGE_MK_2 = 'Storage Mk. 2';
 export const STORAGE_TANK = 'Storage Tank';
 export const EM_RAIL_EJECTOR = 'EM-Rail Ejector';
 export const PLANETARY_LOGISTICS_STATION = 'Planetary Logistics Station';
-export const MINI_PARTICLE_COLLIDER = 'Mini particle collider';
+export const MINI_PARTICLE_COLLIDER = 'Mini Particle Collider';
 
 // 3
-export const SORTER_MK_1 = 'Sorter MK. 1';
-export const SORTER_MK_2 = 'Sorter MK. 2';
-export const SORTER_MK_3 = 'Sorter MK. 3';
-export const MINING_MACHINE = 'Mining machine';
-export const WATER_PUMP = 'Water pump';
+export const SORTER_MK_1 = 'Sorter Mk. 1';
+export const SORTER_MK_2 = 'Sorter Mk. 2';
+export const SORTER_MK_3 = 'Sorter Mk. 3';
+export const MINING_MACHINE = 'Mining Machine';
+export const WATER_PUMP = 'Water Pump';
 export const OIL_EXTRACTOR = 'Oil Extractor';
-export const OIL_REFINERY = 'Oil refinery';
-export const VERTICAL_LAUNCHING_SILO = 'Vertical launching silo';
+export const OIL_REFINERY = 'Oil Refinery';
+export const VERTICAL_LAUNCHING_SILO = 'Vertical Launching Silo';
 export const INTERSTELLAR_LOGISTICS_STATION = 'Interstellar Logistics Station';
 
 // 4
-export const ASSEMBLING_MACHINE_MK_1 = 'Assembling machine MK. 1';
-export const ASSEMBLING_MACHINE_MK_2 = 'Assembling machine MK. 2';
-export const ASSEMBLING_MACHINE_MK_3 = 'Assembling machine MK. 3';
+export const ASSEMBLING_MACHINE_MK_1 = 'Assembling machine Mk. 1';
+export const ASSEMBLING_MACHINE_MK_2 = 'Assembling machine Mk. 2';
+export const ASSEMBLING_MACHINE_MK_3 = 'Assembling machine Mk. 3';
 export const SMELTER = 'Smelter';
 export const FRACTIONATOR = 'Fractionator';
-export const CHEMICAL_PLANT = 'Chemical plant';
+export const CHEMICAL_PLANT = 'Chemical Plant';
 export const MATRIX_LAB = 'Matrix Lab';
 export const ORBIT_COLLECTOR = 'Orbit Collector';
